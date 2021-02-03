124cfb5bf77349737a0796e464b7973f8745a987
Pavel OptimalSolution <pavel.slajnev@email.com>
2021-02-03 14:45:37 +0200

    Update

--- devop/deploy.sh
+++ devop/deploy.sh
@@ -1,12 +1,8 @@
-
-docker stop first_nginx
-docker stop second_nginx
-docker rmi -f first_nginx second_nginx
-cd ./first/
-docker-compose build
-cd ../second/
-docker-compose build
+docker stop $(docker ps -a -q)
+docker rm $(docker ps -a -q)
+#docker rmi -f $(docker images -q)
+docker-compose -f first/docker-compose.yaml  build
+docker-compose -f second/docker-compose.yaml  build
 docker run -it --rm -d -p 8090:80 --cpus=1 -m 512m --memory-reservation=256m --name first_nginx first_nginx
 docker run -it --rm -d -p 8100:80 --cpus=1 -m 512m --memory-reservation=256m --name second_nginx second_nginx
-cd ../
 
