d76eb48a31c4ce9458c8994bad05db6cc850fcce
Pavel OptimalSolution <pavel.slajnev@email.com>
2021-02-03 15:43:37 +0200

    Update

--- devop/deploy.sh
+++ devop/deploy.sh
@@ -1,8 +1,10 @@
 docker stop first_nginx second_nginx
 docker rm -f first_nginx second_nginx
-cd first
+cd ./first/
+ls
 docker-compose  build
-cd ../second
+cd ../second/
+ls
 docker-compose  build
 cd ../
 docker run -it --rm -d -p 8090:80 --cpus=1 -m 512m --memory-reservation=256m --name first_nginx first_nginx
